c5fb333c458f861290e7475a88bc6fec87940c7a
L.N <516421987@qq.com>
2019-03-21 13:33:33 +0800

    精简脚本,修改部分功能

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -7,49 +7,6 @@ fi
 
 ### Functions ###
 
-debian_initialize() {
-	echo "Updating and Installing Dependicies"
-	apt-get -qq update > /dev/null 2>&1
-	apt-get -qq -y upgrade > /dev/null 2>&1
-	apt-get install -qq -y nmap > /dev/null 2>&1
-	apt-get install -qq -y git > /dev/null 2>&1
-	apt-get remove -qq -y exim4 exim4-base exim4-config exim4-daemon-light > /dev/null 2>&1
-	rm -r /var/log/exim4/ > /dev/null 2>&1
-
-	update-rc.d nfs-common disable > /dev/null 2>&1
-	update-rc.d rpcbind disable > /dev/null 2>&1
-
-	echo "IPv6 Disabled"
-
-	cat <<-EOF >> /etc/sysctl.conf
-	net.ipv6.conf.all.disable_ipv6 = 1
-	net.ipv6.conf.default.disable_ipv6 = 1
-	net.ipv6.conf.lo.disable_ipv6 = 1
-	net.ipv6.conf.eth0.disable_ipv6 = 1
-	net.ipv6.conf.eth1.disable_ipv6 = 1
-	net.ipv6.conf.ppp0.disable_ipv6 = 1
-	net.ipv6.conf.tun0.disable_ipv6 = 1
-	EOF
-
-	sysctl -p > /dev/null 2>&1
-
-	echo "Changing Hostname"
-
-	read -p "Enter your hostname: " -r primary_domain
-
-	cat <<-EOF > /etc/hosts
-	127.0.1.1 $primary_domain $primary_domain
-	127.0.0.1 localhost
-	EOF
-
-	cat <<-EOF > /etc/hostname
-	$primary_domain
-	EOF
-
-	echo "The System will now reboot!"
-	reboot
-}
-
 ubuntu_initialize() {
 	echo "Updating and Installing Dependicies"
 	apt-get -qq update > /dev/null 2>&1
@@ -61,18 +18,6 @@ ubuntu_initialize() {
 	update-rc.d nfs-common disable > /dev/null 2>&1
 	update-rc.d rpcbind disable > /dev/null 2>&1
 
-	echo "IPv6 Disabled"
-
-	cat <<-EOF >> /etc/sysctl.conf
-	net.ipv6.conf.all.disable_ipv6 = 1
-	net.ipv6.conf.default.disable_ipv6 = 1
-	net.ipv6.conf.lo.disable_ipv6 = 1
-	net.ipv6.conf.eth0.disable_ipv6 = 1
-	net.ipv6.conf.eth1.disable_ipv6 = 1
-	net.ipv6.conf.ppp0.disable_ipv6 = 1
-	net.ipv6.conf.tun0.disable_ipv6 = 1
-	EOF
-
 	sysctl -p > /dev/null 2>&1
 
 	echo "Changing Hostname"
@@ -87,82 +32,8 @@ ubuntu_initialize() {
 	cat <<-EOF > /etc/hostname
 	$primary_domain
 	EOF
+	hostname $primary_domain
 
-	echo "The System will now reboot!"
-	reboot
-}
-
-
-reset_firewall() {
-	apt-get install iptables-persistent -q -y > /dev/null 2>&1
-
-	iptables -F
-	echo "Current iptables rules flushed"
-	cat <<-ENDOFRULES > /etc/iptables/rules.v4
-	*filter
-
-	# Allow all loopback (lo) traffic and reject anything to localhost that does not originate from lo.
-	-A INPUT -i lo -j ACCEPT
-	-A INPUT ! -i lo -s 127.0.0.0/8 -j REJECT
-	-A OUTPUT -o lo -j ACCEPT
-
-	# Allow ping and ICMP error returns.
-	-A INPUT -p icmp -m state --state NEW --icmp-type 8 -j ACCEPT
-	-A INPUT -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT
-	-A OUTPUT -p icmp -j ACCEPT
-
-	# Allow SSH.
-	-A INPUT -i  eth0 -p tcp -m state --state NEW,ESTABLISHED --dport 22 -j ACCEPT
-	-A OUTPUT -o eth0 -p tcp -m state --state NEW,ESTABLISHED --sport 22 -j ACCEPT
-
-	# Allow DNS resolution and limited HTTP/S on eth0.
-	# Necessary for updating the server and keeping time.
-	-A INPUT  -p udp -m state --state NEW,ESTABLISHED --sport 53 -j ACCEPT
-	-A OUTPUT  -p udp -m state --state NEW,ESTABLISHED --dport 53 -j ACCEPT
-	-A INPUT  -p tcp -m state --state ESTABLISHED --sport 80 -j ACCEPT
-	-A INPUT  -p tcp -m state --state ESTABLISHED --sport 443 -j ACCEPT
-	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 80 -j ACCEPT
-	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 443 -j ACCEPT
-
-	# Allow Mail Server Traffic outbound
-	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 143 -j ACCEPT
-	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 587 -j ACCEPT
-	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 993 -j ACCEPT
-	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 25 -j ACCEPT
-
-	# Allow Mail Server Traffic inbound
-	-A INPUT  -p tcp -m state --state NEW,ESTABLISHED --sport 143 -j ACCEPT
-	-A INPUT  -p tcp -m state --state NEW,ESTABLISHED --sport 587 -j ACCEPT
-	-A INPUT  -p tcp -m state --state NEW,ESTABLISHED --sport 993 -j ACCEPT
-	-A INPUT  -p tcp -m state --state NEW,ESTABLISHED --sport 25 -j ACCEPT
-
-	COMMIT
-	ENDOFRULES
-
-	iptables -P INPUT DROP
-	iptables -P FORWARD DROP
-	iptables -P OUTPUT DROP
-
-	cat <<-ENDOFRULES > /etc/iptables/rules.v6
-	*filter
-
-	-A INPUT -j DROP
-	-A FORWARD -j DROP
-	-A OUTPUT -j DROP
-
-	COMMIT
-	ENDOFRULES
-
-	echo "Loading new firewall rules"
-	iptables-restore /etc/iptables/rules.v4
-	ip6tables-restore /etc/iptables/rules.v6
-}
-
-add_firewall_port(){
-	read -p "Enter the port you would like opened: " -r port
-	iptables -A INPUT -p tcp --dport ${port} -j ACCEPT
-	iptables -A OUTPUT -p tcp --sport ${port} -j ACCEPT
-	iptables-save
 }
 
 
@@ -385,7 +256,8 @@ function add_alias(){
 }
 
 function get_dns_entries(){
-	extip=$(ifconfig|grep 'Link encap\|inet '|awk '!/Loopback|:127./'|tr -s ' '|grep 'inet'|tr ':' ' '|cut -d" " -f4)
+	#extip=$(ifconfig|grep 'Link encap\|inet '|awk '!/Loopback|127./'|tr -s ' '|grep 'inet'|tr ':' ' '|cut -d" " -f3)
+	extip="{your internet ipv4}"
 	domain=$(ls /etc/opendkim/keys/ | head -1)
 	fields=$(echo "${domain}" | tr '.' '\n' | wc -l)
 	dkimrecord=$(cut -d '"' -f 2 "/etc/opendkim/keys/${domain}/mail.txt" | tr -d "[:space:]")
@@ -449,7 +321,7 @@ function get_dns_entries(){
 		TTL: 5 min
 
 		Record Type: TXT
-		Host: ._dmarc
+		Host: _dmarc
 		Value: v=DMARC1; p=reject
 		TTL: 5 min
 
@@ -462,65 +334,6 @@ function get_dns_entries(){
 		EOF
 		cat dnsentries.txt
 	fi
-
-}
-
-setupSSH(){
-	apt-get -qq -y install sudo > /dev/null 2>&1
-	apt-get -qq -y install fail2ban > /dev/null 2>&1
-
-	echo "Create a User to ssh into this system securely"
-
-	read -p "Enter your user name: " -r user_name
-
-	adduser $user_name
-
-	usermod -aG sudo $user_name
-
-	cat <<-EOF > /etc/ssh/sshd_config
-	Port 22
-	Protocol 2
-	HostKey /etc/ssh/ssh_host_rsa_key
-	HostKey /etc/ssh/ssh_host_dsa_key
-	HostKey /etc/ssh/ssh_host_ecdsa_key
-	#Privilege Separation is turned on for security
-	UsePrivilegeSeparation yes
-	KeyRegenerationInterval 3600
-	ServerKeyBits 1024
-	SyslogFacility AUTH
-	LogLevel INFO
-	LoginGraceTime 120
-	PermitRootLogin no
-	StrictModes yes
-	RSAAuthentication yes
-	PubkeyAuthentication yes
-	IgnoreRhosts yes
-	RhostsRSAAuthentication no
-	HostbasedAuthentication no
-	PermitEmptyPasswords no
-	ChallengeResponseAuthentication no
-	PasswordAuthentication yes
-	X11Forwarding yes
-	X11DisplayOffset 10
-	PrintMotd no
-	PrintLastLog yes
-	TCPKeepAlive yes
-	Banner no
-	AcceptEnv LANG LC_*
-	Subsystem sftp /usr/lib/openssh/sftp-server
-	UsePAM yes
-	EOF
-
-	echo "AllowUsers ${user_name}" > /etc/ssh/sshd_config
-
-	cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-
-	cd /home/$user_name
-	runuser -l $user_name -c "mkdir '.ssh'"
-	runuser -l $user_name -c "chmod 700 ~/.ssh"
-
-	service ssh restart
-
 }
 
 function Install_GoPhish {
@@ -566,29 +379,20 @@ function Install_IRedMail {
 }
 
 PS3="Server Setup Script - Pick an option: "
-options=("Setup SSH" "Debian Prep" "Ubuntu Prep" "Install SSL" "Install Mail Server" "Add Aliases" "Get DNS Entries" "Install GoPhish" "Install IRedMail")
+options=("Ubuntu Prep" "Install SSL" "Install Mail Server" "Add Aliases" "Get DNS Entries")
 select opt in "${options[@]}" "Quit"; do
 
     case "$REPLY" in
-
     #Prep
-    1) setupSSH;;
+		1) ubuntu_initialize;;
 
-		2) debian_initialize;;
+		2) install_ssl_Cert;;
 
-		3) ubuntu_initialize;;
+		3) install_postfix_dovecot;;
 
-		4) install_ssl_Cert;;
+		4) add_alias;;
 
-		5) install_postfix_dovecot;;
-
-		6) add_alias;;
-
-		7) get_dns_entries;;
-
-		8) Install_GoPhish;;
-
-		9) Install_IRedMail;;
+		5) get_dns_entries;;
 
     $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
     *) echo "Invalid option. Try another one.";continue;;
